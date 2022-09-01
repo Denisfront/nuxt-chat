e9beebe1567a2dbfa48f6145a84e30723a95fed6
Denis <smaylik911@gmail.com>
2022-09-01 15:03:07 +0700

    feat: add socket client

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
   <v-row justify="center" align="center">
-    <v-btn>Привет</v-btn>
+    <v-btn @click="handler">Привет</v-btn>
+    <p>{{ message }}</p>
   </v-row>
 </template>
 
 <script>
+import { socket } from '@/services/socket'
+
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      message: '',
+    }
+  },
+  mounted() {
+    socket.on('connect', () => {
+      console.log(socket.connected)
+    })
+
+    socket.on('response', ({ text }) => {
+      this.message = text
+    })
+  },
+  methods: {
+    handler() {
+      socket.emit('new_message', { text: 'Привет, сервер' })
+    },
+  },
 }
 </script>
